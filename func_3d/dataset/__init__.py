b1a68b0a6e125d84dbdc8de69e0acf0e0c595226
Yang Xing <yxing2@c1009a-s11.ufhpc>
2024-12-27 09:54:17 -0500

    initial commit

---
+++ func_3d/dataset/__init__.py
@@ -0,0 +1,73 @@
+from .btcv import BTCV
+from .amos import AMOS
+from .pet_tumor import PETCT
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
+
+
+
+def get_dataloader(args):
+    # transform_train = transforms.Compose([
+    #     transforms.Resize((args.image_size,args.image_size)),
+    #     transforms.ToTensor(),
+    # ])
+
+    # transform_train_seg = transforms.Compose([
+    #     transforms.Resize((args.out_size,args.out_size)),
+    #     transforms.ToTensor(),
+    # ])
+
+    # transform_test = transforms.Compose([
+    #     transforms.Resize((args.image_size, args.image_size)),
+    #     transforms.ToTensor(),
+    # ])
+
+    # transform_test_seg = transforms.Compose([
+    #     transforms.Resize((args.out_size,args.out_size)),
+    #     transforms.ToTensor(),
+    # ])
+    
+    if args.dataset == 'btcv':
+        '''btcv data'''
+        btcv_train_dataset = BTCV(args, args.data_path, transform = None, transform_msk= None, mode = 'Training', prompt=args.prompt)
+        btcv_test_dataset = BTCV(args, args.data_path, transform = None, transform_msk= None, mode = 'Test', prompt=args.prompt)
+
+        nice_train_loader = DataLoader(btcv_train_dataset, batch_size=4, shuffle=True, num_workers=4, pin_memory=True)
+        nice_test_loader = DataLoader(btcv_test_dataset, batch_size=1, shuffle=False, num_workers=2, pin_memory=True)
+        '''end'''
+    elif args.dataset == 'amos':
+        '''amos data'''
+        amos_train_dataset = AMOS(args, args.data_path, transform = None, transform_msk= None, mode = 'Training', prompt=args.prompt)
+        amos_test_dataset = AMOS(args, args.data_path, transform = None, transform_msk= None, mode = 'Test', prompt=args.prompt)
+
+        nice_train_loader = DataLoader(amos_train_dataset, batch_size=1, shuffle=True, num_workers=8, pin_memory=True)
+        nice_test_loader = DataLoader(amos_test_dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+        '''end'''
+    elif args.dataset == 'petct':
+        petct_train_dataset = PETCT(args, args.data_path, transform=None, transform_msk=None, mode='Training',
+                                  prompt=args.prompt)
+        petct_test_dataset = PETCT(args, args.data_path, transform=None, transform_msk=None, mode='Testing',
+                                 prompt=args.prompt)
+        dataset_size = len(petct_train_dataset)
+        indices = list(range(dataset_size))
+        split_support = 1
+        test_dataset_size = len(petct_test_dataset)
+        indices_test = list(range(test_dataset_size))
+        split_val = int(np.floor(0.5*test_dataset_size))
+        np.random.shuffle(indices)
+        np.random.shuffle(indices_test)
+        train_sampler = SubsetRandomSampler(indices[split_support:])
+        support_sampler = SubsetRandomSampler(indices[:split_support])
+        val_sampler = SubsetRandomSampler(indices_test[split_val:])
+        test_sampler = SubsetRandomSampler(indices_test[:split_val])
+        nice_support_loader = DataLoader(petct_train_dataset, batch_size=1, sampler=support_sampler, num_workers=8, pin_memory=False)
+        nice_train_loader = DataLoader(petct_train_dataset, batch_size=args.b, sampler=train_sampler, num_workers=8, pin_memory=False)
+        nice_test_loader = DataLoader(petct_test_dataset, batch_size=args.b, sampler=test_sampler, num_workers=8, pin_memory=False)
+        nice_val_loader = DataLoader(petct_test_dataset, batch_size=args.b, sampler=val_sampler, num_workers=8,
+                                     pin_memory=False)
+    else:
+        print("the dataset is not supported now!!!")
+        
+    return nice_train_loader, nice_test_loader, nice_support_loader, nice_val_loader
